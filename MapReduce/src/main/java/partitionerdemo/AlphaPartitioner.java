810c6b538ce46f77b4d28cfeddd1a50b144c7a68
innovchn <innovchn@innovchn2>
2016-02-04 18:43:08 +0530

    partitioner

--- MapReduce/src/main/java/partitionerdemo/AlphaPartitioner.java
+++ MapReduce/src/main/java/partitionerdemo/AlphaPartitioner.java
@@ -1,7 +1,47 @@
 package partitionerdemo;
 
-public class AlphaPartitioner {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AlphaPartitioner extends Partitioner<Text, IntWritable> {
+
+	@Override
+	public int getPartition(Text key, IntWritable count, int noOfPartitions) {
+		
+		int partitionNum = 0 ;
+		
+		switch (key.toString()) {
+		
+		case "a":
+			partitionNum = 0;
+			break;
+		case "b":
+			partitionNum = 1;
+			break ;
+		case "c":
+			partitionNum = 2;
+			break ;
+		case "d":
+			partitionNum = 3;
+			break ;
+		case "e":
+			partitionNum = 4;
+			break ;
+		
+		case "f":
+			partitionNum = 5;
+			break ;
+			
+		default:
+			partitionNum = 5;
+			break;
+		}
+		
+		return partitionNum;
+		
+	}
 
 	
-	
+
 }
